6bd728cccfb635ff801731356191bceaa3868f1d
Vasco Ramos <vasco@tyba.com>
2016-05-24 15:40:53 +0200

    adding more commands

--- src/commands/opportunities.js
+++ src/commands/opportunities.js
@@ -16,6 +16,7 @@ const headers = {
 };
 
 var page_size = 50;
+var pipeline_id = 30164;
 
 var options = {
   url: 'https://api.prosperworks.com/developer_api/v1/opportunities/search',
@@ -25,35 +26,54 @@ var options = {
       "sort_by": "status",
       "sort_direction":"asc",
       "page_size": page_size,
-      "pipeline_ids": [30164]
+      "pipeline_ids[]": pipeline_id,
   }
 };
 
-// There is a better way to do this :)
-
-/* Things to keep in mind is the sort direction needs to be perserved for this to work */
-opportunities.getOpportunitiesCount = function(func){
+opportunities.openOpportunitiesCount = function(func){
     var i = 1, nOpportunities = 0;
+    options.form["page_number"] = i;
     function getResponse(error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log("here 3");
             var filtered = _.filter(body,['status','Open']);
             nOpportunities += filtered.length;
             if((nOpportunities/i) == page_size){
-                console.log("here 4");
-                i++;
-                options.form["page_number"] = i;
+                options.form["page_number"] = ++i;
                 request.post(options, getResponse); // Things will be kept memory, consider changing this
             } else {
-                console.log("here 5");
-                func(nOpportunities)
+                func(nOpportunities);
             }
         } else {
-            console.log(response);
         }
     }
-    request.post(options, getResponse)
+    request.post(options, getResponse);
 }
+
+
+opportunities.closingOpportunitiesCount = function(func){
+    var i = 1, nOpportunities = 0;
+    options.form["page_number"] = i;
+    function getResponse(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var filtered = _.filter(body,['status','Open']);
+            nOpportunities += filtered.length;
+            if((nOpportunities/i) == page_size){
+                options.form["page_number"] = ++i;
+                request.post(options, getResponse); // Things will be kept memory, consider changing this
+            } else {
+                func(nOpportunities);
+            }
+        } else {
+        }
+    }
+    request.post(options, getResponse);
+}
+
+
+opportunities.command =function(message){
+
+}
+
 
 
 module.exports = opportunities
