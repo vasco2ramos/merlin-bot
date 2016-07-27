0f6e05305531528fc1575932e60a16358df99499
Vasco Ramos <vascoramos@outlook.com>
2016-07-27 14:22:01 +0200

    cleaner code, double yay

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 'use strict'
 
-const slack = require('slack')
-const _ = require('lodash')
 const botkit = require('botkit')
 const config = require('./config')
 var os = require('os');
 
 const commands = require('./commands')
+const seneca = require('seneca')
 
 var controller = botkit.slackbot({
     debug: false,
@@ -24,10 +23,6 @@ controller.hears(['hello'], 'direct_message,direct_mention,mention', function(bo
     bot.reply(message, 'Hello.');
 });
 
-controller.hears(['release the hounds'], 'direct_message,direct_mention,mention', function(bot, message) {
-    bot.reply(message, 'Excelent!');
-});
-
 controller.hears(['help', 'halp'], 'direct_message,direct_mention,mention', function(bot, message) {
     var reply = {
         'attachments': commands.help()
@@ -36,43 +31,12 @@ controller.hears(['help', 'halp'], 'direct_message,direct_mention,mention', func
 });
 
 controller.hears(['opportunities'], 'direct_message,direct_mention,mention', function(bot, message) {
-    const callback = function(response) {
-            bot.reply(message, response);
-        }
-    commands.opportunities.cmd(message.text, callback)
+    opportunities(message.text)
 });
 
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
-    'direct_message,direct_mention,mention', function(bot, message) {
 
-        var hostname = os.hostname();
-        var uptime = formatUptime(process.uptime());
 
-        bot.reply(message,
-            ':robot_face: I am a bot named <@' + bot.identity.name +
-             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-
-    });
 
 function printResponse(response, status, message){
-    bot.reply(message, " - " + response)
-}
-
-
-function formatUptime(uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
-    }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if (uptime != 1) {
-        unit = unit + 's';
-    }
-
-    uptime = uptime + ' ' + unit;
-    return uptime;
+    bot.reply(message, response)
 }
