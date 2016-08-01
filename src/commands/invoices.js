d1ea61fbc0b66fcb8ce2cd1008c34a7d59371c3a
Vasco Ramos <vascoramos@outlook.com>
2016-08-01 17:01:12 +0200

    continuing

--- src/commands/invoices.js
+++ src/commands/invoices.js
@@ -6,8 +6,8 @@ const prosperworks = require('../helpers/prosperworks');
 module.exports = function invoices( options ) {
   var seneca = this;
 
-  seneca.add('role: invoices, cmd: status', status);
-  seneca.add('role: invoices, cmd: cancel', cancel);
+  seneca.add({cmd: 'status'}, status);
+  seneca.add({cmd: 'cancel'}, cancel);
 
   function status( args, done ) {
     done( null, {text:'foo-'+args.text+suffix} )
